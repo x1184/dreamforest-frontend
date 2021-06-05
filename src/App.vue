e17978e799b32954f5d462ebb323e0cc1031620f
guang <guangluo@outlook.com>
2021-06-05 17:38:32 +0800

    remove all files

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,5 @@
 <template>
   <div>
-    <router-view></router-view>
-    <router-view name="footer"></router-view>
+    Hello DreamForest
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-import DfFooter from './layouts/DfFooter.vue'
-
-export default defineComponent({
-  components: {
-    [DfFooter.name]: DfFooter
-  }
-})
-</script>
--- src/routers/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import routers from './router'
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: routers
-})
-// 可以进行路由守卫
-export default router
