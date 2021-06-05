158a3b358dfc0103a86d4b4b036ac768e58cda57
guang <guangluo@outlook.com>
2021-06-05 17:42:16 +0800

    remove some packages

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,4 @@
 import { createApp } from 'vue'
-import 'animate.css'
-
-import router from './routers'
-
 import App from './App.vue'
-import './utils/rem'
 
-const Vue = createApp(App)
-
-Vue.use(router)
-
-Vue.mount('#app')
+createApp(App).mount('#app')
